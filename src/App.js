84c1bf809332d13d90e37afd7023d00e4901a5af
Guadalupe Recio <guadaluperecio@gmail.com>
2023-01-09 13:38:53 +0100

    Sidebar y routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 import Ordenes from "./Components/Paginas/Ordenes";
+import Menu from "./Components/Paginas/Menu";
+import NuevoPlato from "./Components/Paginas/Nuevoplato";
+import Sidebar from "./Components/UI/Sidebar";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Ordenes />} />
-      </Routes>
+    <div className="md:flex min-h-screen">
+      <Sidebar />
+      <div className="md:w-3/5 xl:w-4/5">
+        <Routes>
+          <Route path="/" element={<Ordenes />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/nuevo-plato" element={<NuevoPlato />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Paginas/Nuevoplato.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NuevoPlato = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-light mb-4">NuevoPlato </h1>
+    </>
+  );
+};
+
+export default NuevoPlato;
